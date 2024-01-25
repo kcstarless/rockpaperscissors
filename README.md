88f411245ba8ccadf66ccd0f09db5e65d3f092d8
kcstarless <155853062+kcstarless@users.noreply.github.com>
2024-01-25 11:16:16 +1100

    Completed the code. It's in working order.

--- README.md
+++ README.md
@@ -1,3 +1,11 @@
 # rockpaperscissors
-"This is the third project from TOP - Console based 'Rock Paper Scissors' game."
+"This is the third project from TOP - Simple console based 'Rock Paper Scissors' game using javascript."
+    Prompt users to choose between Rock Paper Sicssors and 
+    opponent, a computer, choice is made using random number.
 
+    Runs the game 5 times to pick out the winner. If the game is a tie then it will reprompt for user input.
+    If user dosen't enter any value, ie null, it will prompt user input again.
+
+    Each game will decide a winner and update the score. 
+
+    After 5 games it will print the winner in the console. 
--- javascript.js
+++ javascript.js
@@ -1,3 +1,51 @@
+let numberOfGames = 5;
+let playerScore = 0;
+let compScore = 0;
+
 function getComputerChoice() {
-    
+    const choiceOfHand = ["Rock", "Paper", "Scissors"];
+    let compHand = choiceOfHand[Math.floor(Math.random() * 3)];
+    return compHand.toLowerCase();
 }
+
+function getPlayerChoice() {
+    let playerHand = null;
+    while (playerHand === null){
+        playerHand = prompt("Rock Paper or Scissors?");
+    }
+    return playerHand.toLowerCase(); 
+}
+
+function playGame(player, comp) {
+    if (player === comp) {
+        console.log("It's a Tie! Play again.");
+        playerHand = null;
+        playGame(getPlayerChoice(), getComputerChoice());
+    }
+    else if ((player === "rock" && comp === "scissors") ||
+             (player === "paper" && comp === "rock") ||
+             (player === "scissors" && comp === "paper")) {
+        
+        console.log("You Win! " + player + " beats " + comp);
+        playerScore++;
+    }
+    else {
+        console.log("You Lose! " + comp + " beats " + player);
+        compScore++;
+    }    
+}
+
+function game() {
+    for (let i = 0; i < numberOfGames; i++ ) {
+        playGame(getPlayerChoice(), getComputerChoice());
+        playerHand = null;
+    }    
+
+    if (playerScore > compScore) {
+        console.log("Player Wins!: Score: " + playerScore + " | " + compScore);
+    }
+    else {
+        console.log("Computer Wins!: Score: " + compScore + " | " + playerScore);
+    }
+}
+game();
